f58ee6f1c4c3650f34bb63cfc5b54cf87c140ce5
Leeseul-Moon <leeseulmoon4@gmail.com>
2022-11-26 23:15:19 +0900

    Feat: click 시 detail 로 이동

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
 import { Video } from '../models/Video';
 import { formatAgo } from '../util/data';
 
@@ -8,8 +9,13 @@ interface Props {
 
 function VideoCard({ video }: Props) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
   return (
-    <li>
+    <li
+      onClick={() => {
+        navigate(`videos/watch/${video.id}`, { state: { video } });
+      }}
+    >
       <img className="w-full" src={thumbnails.medium.url} alt={title} />
       <div>
         <p className="font-semibold my-2 line-clamp-2">{title}</p>
--- src/pages/VideoDetail.tsx
+++ src/pages/VideoDetail.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useLocation } from 'react-router';
 
 export default function VideoDetail() {
+  const {
+    state: { video },
+  } = useLocation();
+  console.log('video :>> ', video);
   return <div>VideoDetail</div>;
 }
